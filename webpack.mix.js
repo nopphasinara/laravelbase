6c226823b8f7dc56661134ac58a1dcc22ead45b3
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-02-08 04:36:16 +0700

    Update source

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/dashboard/js');
+mix.sass('resources/sass/app.scss', 'public/dashboard/css');
+// mix.js('resources/dashboard/js/app.js', 'public/dashboard/js');
+// mix.sass('resources/dashboard/sass/app.scss', 'public/dashboard/css');
